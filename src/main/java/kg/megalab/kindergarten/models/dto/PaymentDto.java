1203bd2c371e14197c264ab0a402d0b0e53e6500
User <@bolotbekovalibek827@gmail.com>
2025-10-01 23:42:02 +0600

    Add Payment entity and DTO with validation annotations

---
+++ src/main/java/kg/megalab/kindergarten/models/dto/PaymentDto.java
@@ -0,0 +1,27 @@
+package kg.megalab.kindergarten.models.dto;
+
+import jakarta.validation.constraints.NotNull;
+import jakarta.validation.constraints.Positive;
+import lombok.AccessLevel;
+import lombok.AllArgsConstructor;
+import lombok.Data;
+import lombok.NoArgsConstructor;
+import lombok.experimental.FieldDefaults;
+
+import java.time.LocalDate;
+
+@Data
+@NoArgsConstructor
+@AllArgsConstructor
+@FieldDefaults(level = AccessLevel.PRIVATE)
+public class PaymentDto {
+    @NotNull(message = "ID записи ребенка в группе обязателен")
+    Long groupChildrenId;
+
+    @NotNull(message = "Сумма платежа обязательна")
+    @Positive(message = "Сумма должна быть положительной")
+    Integer amount;
+
+    @NotNull(message = "Дата платежа обязательна")
+    LocalDate paymentDate;
+}
