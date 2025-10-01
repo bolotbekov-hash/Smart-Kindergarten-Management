6abb2e12bf92eac054c5b15b240cedb7f7d7cd1f
User <@bolotbekovalibek827@gmail.com>
2025-10-01 21:26:21 +0600

    Add Teacher entity, DTO and validation annotations

---
+++ src/main/java/kg/megalab/kindergarten/models/dto/TeacherCreateDto.java
@@ -0,0 +1,28 @@
+package kg.megalab.kindergarten.models.dto;
+
+import jakarta.validation.constraints.NotBlank;
+import jakarta.validation.constraints.NotNull;
+import kg.megalab.kindergarten.models.Enum.TeacherDegree;
+import lombok.AccessLevel;
+import lombok.AllArgsConstructor;
+import lombok.Data;
+import lombok.NoArgsConstructor;
+import lombok.experimental.FieldDefaults;
+
+
+
+@FieldDefaults(level = AccessLevel.PRIVATE)
+@Data
+@AllArgsConstructor
+@NoArgsConstructor
+public class TeacherCreateDto {
+    @NotBlank(message = "Имя обязательно")
+    String firstName;
+    @NotBlank(message = "Фамилия обязательна")
+    String lastName;
+    String patronymic;
+    @NotBlank(message = "Роль обязательна")
+    TeacherDegree teacherDegree;
+    @NotNull(message = "Статус обязателен")
+    Boolean active;
+}
